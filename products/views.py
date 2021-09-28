bda8a0d7ecd1501b0b46f23029fb474f29e92b08
rlopezba1 <rubenlopezb@gmail.com>
2021-09-28 14:16:41 +0000

    Product Admin - Fixing the image field part 1

--- products/views.py
+++ products/views.py
@@ -100,6 +100,9 @@ def add_product(request):
 @login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
