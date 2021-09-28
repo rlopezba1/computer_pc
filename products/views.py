760527f3acf2b341567452c7fec935c3c5fd48dd
rlopezba1 <rubenlopezb@gmail.com>
2021-09-28 09:46:02 +0000

    Added edit and delete products

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -115,7 +115,7 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
-@login_required
+
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
