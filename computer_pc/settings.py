aaec13242b2811d049df04c2579b33a8327fd00f
rlopezba1 <rubenlopezb@gmail.com>
2021-09-30 12:41:03 +0000

    Deployment data in heroku and creating json files

--- computer_pc/settings.py
+++ computer_pc/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,13 +123,16 @@ WSGI_APPLICATION = 'computer_pc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://dqcsppjyyydosd:89c1f5abc8ea7588618f052e28f300095952bd997cf54c2cbd6669734e18d6d2@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/dbdkep1l1vnel7')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
