12966895b93abd7aef2580e53b5d23664df11bdb
rlopezba1 <rubenlopezb@gmail.com>
2021-09-30 13:27:12 +0000

    Removed secret key and set debug

--- computer_pc/settings.py
+++ computer_pc/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uewmjz6z8_g74^j+1s@tr*@26eb$yxsdz6y+vzgxmr$qk^7g&m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ruben-computer-pc.herokuapp.com', 'localhost']
 
